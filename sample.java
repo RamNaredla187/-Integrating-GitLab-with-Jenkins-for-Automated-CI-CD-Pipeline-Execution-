95e773d758b96642c28f4129c8174a8daab296bb
RamNaredla187 <ramnaredla4@gmail.com>
2025-01-20 18:24:47 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,15 @@
+// 1/Sample Java Program
+
+// Define a public class named CICDSuccess
+public class sample {
+
+    /**
+     * The main method serves as the entry point of the Java program.
+     * @param args Command-line arguments passed as an array of strings.
+     */
+    public static void main(String[] args) {
+        // Print a success message for the CI/CD pipeline
+        System.out.println("Sample CI/CD pipeline successfully created and executed!");
+    }
+}
+
